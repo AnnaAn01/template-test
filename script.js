fbcb9bcc66bee9dbb470773df5d101771da19478
Anna An <arzumanyan.an@gmail.com>
2021-12-20 18:25:38 +0400

    Added the local version without the API

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 let apiQuotes = [];
 
+// Show new Quote
+function newQuote() {
+  // Pick a random quote from apiQuotes array
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  console.log(quote);
+}
+
 // Get quotes from API
 async function getQuotes() {
   const apiUrl = "https://type.fit/api/quotes";
   try {
     const response = await fetch(apiUrl);
     apiQuotes = await response.json();
-    console.log(apiQuotes[12]);
+    newQuote();
   } catch (error) {
     // Catch error here
   }
@@ -14,3 +21,30 @@ async function getQuotes() {
 
 // On Load
 getQuotes();
+
+//
+//
+//
+// The local version
+// // Show new Quote
+// function newQuote() {
+//     // Pick a random quote from apiQuotes array
+//     const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
+//     console.log(quote);
+//   }
+
+//   // // Get quotes from API
+//   // async function getQuotes() {
+//   //   const apiUrl = "https://type.fit/api/quotes";
+//   //   try {
+//   //     const response = await fetch(apiUrl);
+//   //     apiQuotes = await response.json();
+//   //     newQuote();
+//   //   } catch (error) {
+//   //     // Catch error here
+//   //   }
+//   // }
+
+//   // On Load
+//   newQuote();
+//   // getQuotes();
